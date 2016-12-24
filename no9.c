1d1aa3dff40299c469e38c5a41ffdc69c27bd0ad
ToM-ynu <tanaka-tomoyuki-wy@ynu.jp>
2016-12-24 23:50:21 +0900

    fix goto hogehoge to goto loop

--- no9.c
+++ no9.c
@@ -2,16 +2,16 @@
 
 int main(){
   int num;
-hogehoge:
+loop:
   scanf("%d",&num);
   if(num==-1) goto out;
   else if(num%2==0){
     printf("even\n");
-    goto hogehoge;
+    goto loop;
   }
   else if(num%2==1){
     printf("odd\n");
-    goto hogehoge;
+    goto loop;
   }
 out:
   return 0;
